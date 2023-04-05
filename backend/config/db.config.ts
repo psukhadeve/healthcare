ed5ea87fe4e8e81a28370fefa2ccaa592baf8006
wa5 <waseemslyar@gmail.com>
2023-04-05 14:50:08 +0530

    changed the db settings for the production mode

--- backend/config/db.config.ts
+++ backend/config/db.config.ts
@@ -1,6 +1,9 @@
 var mongoose=require('mongoose')
-export var url2="mongodb://127.0.0.1:27017/aisha"
-mongoose.connect(url2,(err:any)=>{
+
+export var url2="mongodb+srv://healthcare:mongo99%23@cluster0.7f2uqw1.mongodb.net/?retryWrites=true&w=majority"
+//export var url2="mongodb://127.0.0.1:27017/aisha"
+let config={ useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(url2,config,(err:any)=>{
     if(err){
        console.log(err)
     }else{
